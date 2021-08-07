8a6b6053c88e7f3c5a0c7a39533ef2efec8f40fb
Suman Baul <musique.suman@gmail.com>
2021-08-07 17:07:33 +0530

    Google tag added for analytics

--- index.php
+++ index.php
@@ -20,6 +20,16 @@
   <!-- Custom Stylesheet -->
   <link rel="stylesheet" href="./css/settings.css">
   <link rel="stylesheet" href="./css/style.css">
+
+   <!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-CJPQZ8TN02"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'G-CJPQZ8TN02');
+	</script>
 </head>
 
 <body>
